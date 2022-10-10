36a1eb1920f2bb23e9b4d304e3dab990e14097e7
jonberard <jtb2125@gmail.com>
2022-10-10 11:43:46 -0500

    Settings class setup

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings
 
 
 # above imports and contains the functionality we need to build on.
@@ -13,15 +14,19 @@ class AlienInvasion:
         """Initialize the game, and create game resources."""
 
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))  # creates display window in px
-        #  variable assignment makes it available in all methods in the class
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))  # creates display window in px
+        # imports the screen width and height from settings file
         #  surface variable - part of screen in PyGame where game element can be displayed
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
         """Start the main loop for the game"""
         #  This method controls the whole game.
+        #  Redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
 
         while True:
             # Watch for keyboard and mouse events.
@@ -29,6 +34,8 @@ class AlienInvasion:
                 #  an event is an action the user performs while playing the game. This event loop listens for events.
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw the screen during each pass through the loop.
 
             # make the most recently drawn screen visible. draws and empty new screen on each loop pass.
             pygame.display.flip()
